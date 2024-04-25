3ab1aa6afa0e141a209083efe9e8e56596d2f781
sonix <sonixmc@gmail.com>
2024-04-25 11:06:24 -0400

    Setup astro hostname 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://manueldurancl.vercel.app",
+  site: "https://manueldurancl-git-main-sonixmcs-projects.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
   output: 'server',
   adapter: vercel(),
